0adeb902559a724840c6eb13089b9be73ce5fdf7
devcube2 <jejeje8@knou.ac.kr>
2024-12-11 18:20:05 +0900

    백엔드 day3 커밋

--- tj2024b_backend/src/day03/Example1.java
+++ tj2024b_backend/src/day03/Example1.java
@@ -1,10 +1,25 @@
 package day03;
 
+import java.util.Scanner;
+
 public class Example1 {
 
 	public static void main(String[] args) {
-		// P.82 1분 복습
-		// 2.
-		
+
+		Scanner scan = new Scanner(System.in);
+
+		int iH = scan.nextInt();
+		int iM = scan.nextInt();
+
+		int tiM = iH * 60 + iM;
+		int toM = tiM - 45;
+		int oH = toM / 60;
+		int oM = toM % 60;
+
+		if (oH > 24) {
+			oH = oH % 24;
+		}
+
+		System.out.print(oH + " " + oM);
 	}
 }
